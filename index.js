120c11dcf051e91e9028854428d3907aecd549e7
cristian.nieto <cristian.nieto@sitrack.com>
2019-12-04 16:29:14 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import {Component} from "react";
+import Mordelon from "mordelon-js";
+
+export default class Source extends Component {
+
+    constructor(props) {
+        super(props);
+        this.source = new Mordelon.Source({ handleDataChange: this.forceUpdate.bind(this), proxy: props.proxy, ...props });
+    }
+
+    setSourceProps() {
+        Object.keys(this.props).forEach((key) => {
+            if (this.source.hasOwnProperty(`_${key}`)) {
+                this.source[key] = this.props[key];
+            }
+        });
+    }
+
+    render() {
+        this.setSourceProps();
+        return (this.props.render && this.props.render(this.source.data));
+    }
+}
